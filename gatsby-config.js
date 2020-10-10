fd2ae446edf65f1d8f971ca86e209fa66276d04c
Jason Huang <jasonhuang20035@gmail.com>
2020-10-10 12:14:17 -0400

    fix: add favicon, update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `JFSS VEX Robotics`,
+        short_name: `JFSS VEX`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#571f86`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-chakra-ui`,
